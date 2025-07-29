c289e60c09139e0cc38ed86f11627b87018fb2c6
TypoweKiwi <wiktorkilian@o2.pl>
2025-07-29 21:33:31 +0200

    Slight game class logic changes

--- Game/Game.py
+++ Game/Game.py
@@ -32,31 +32,38 @@ class Game:
         self.map = Map(players=self.players)
         self.state = Game_state.running
     
-    def save_game_state(self):
+    def save_game_state(self): #TODO eleminate recursion ig stack issues occur 
         save_name = "Team." + self.players.name + "." + datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S') + ".txt"
         save_path = os.path.join(self.save_path, save_name)
         SavesFuncions.save_game(save_path, self.save_path, self.players, self.map)
+        show_message("Game saved successfully.")
+        self.step_menu()
 
     def load_game_state(self):
         pass
-    
-    def move_a_step(self):  #TODO some option after compliting encounter to rest
+
+    def step_menu(self):  #TODO some option after compliting encounter to rest
         choices = [
-            {"name": "Take step", "value": self.map.take_step},
+            {"name": "Take step", "value": lambda: None},
             {"name": "Save game", "value": self.save_game_state}
         ]
         make_query(message="\nChoose next action: ", choices=choices)()
-    
+
     def begin_current_encounter(self):
         self.map.generate_encounter()
         self.map.current_encounter.begin_encounter()
-    
+
+    def take_step(self):
+        self.map.take_step()
+        self.begin_current_encounter()
+        self.step_menu()
+
     def check_state(self):
         if self.map.current_position == self.map.max_steps:
             self.state = Game_state.idle
 
     def check_if_win(self):
-        if self.map.players.get_team_members:
+        if self.map.players.get_team_members():
             show_message("\nYour team completes the adventure. You won!")
         else:
             show_message("\nYour team did not survived. You lost.")
