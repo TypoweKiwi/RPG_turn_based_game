1053bd0bc33d75fa9860e6c9f10fe3e537a93a83
TypoweKiwi <wiktorkilian@o2.pl>
2025-09-10 14:19:08 +0200

    Item generator added

--- PlayerClasses/Inventory/Item.py
+++ PlayerClasses/Inventory/Item.py
@@ -1,20 +1,20 @@
 from PlayerClasses.Stats import Stats
 
 class Item:
-    def __init__(self, item_stats_dict, level=1, rarity="", rarity_factor=1, prefix="", prefix_stats_dict={}):
+    def __init__(self, item_stats_dict, prefix_dict, level=1, rarity=""):
         self.name = item_stats_dict["name"]
         self.desc = item_stats_dict["desc"]
-        self.stats = Stats(item_stats_dict, level)
+        self.stats = Stats(item_stats_dict["basic_stat_dict"], level)
         self.slot = item_stats_dict["slot"]
         self.block_off_hand = item_stats_dict.get("block_off_hand", False)
 
         #Prefix and rarity
         self.rarity = rarity
-        self.prefix = prefix
-        self.prefix_stats = Stats(prefix_stats_dict, level)
+        self.prefix_name = prefix_dict["name"]
+        self.prefix_stats = Stats(prefix_dict["basic_stat_dict"], level)
 
     def get_name(self):
-        return self.rarity + self.prefix + self.name
+        return self.prefix_name + self.name
     
     def get_stats_to_calculate(self):
         return (self.stats + self.prefix_stats)
--- PlayerClasses/Inventory/item_generator.py
+++ PlayerClasses/Inventory/item_generator.py
@@ -1,22 +1,42 @@
+from PlayerClasses.Inventory.Item_and_affixes_lists.prefixes_list import common_prefixes_lst, uncommon_prefixes_lst, rare_prefixes_lst, epic_prefixes_lst, legendary_prefixes_lst
+from PlayerClasses.Inventory.Item_and_affixes_lists.armor_list import armor_templates_list
+from PlayerClasses.Inventory.Item_and_affixes_lists.weapons_list import weapon_templates_list
+from PlayerClasses.Inventory.Item import Item
+import random
+
 class Item_generator:
     def __init__(self):
         self.rarity_affix = {
-            "base": 1,
-            "uncommon": 1.1,
-            "rare": 1.2,
-            "epic": 1.,
-            "legendary":5
+            "Base": common_prefixes_lst,
+            "Uncommon": uncommon_prefixes_lst,
+            "Rare": rare_prefixes_lst,
+            "Epic": epic_prefixes_lst,
+            "Legendary": legendary_prefixes_lst
         }
-    
-    def generate_weapon(self):
-        pass
 
-    def generate_armor(self):
-        pass
+        self.difficulty_table = { #For each of four difficulties there will be diffrent rewards -> (rarities, weights)
+            "short" : (["Base", "Uncommon", "Rare"], [0.5, 0.3, 0.2,]),
+            "medium": (["Base", "Uncommon", "Rare", "Epic"], [0.2, 0.45, 0.3, 0.05]),
+            "long": (["Uncommon", "Rare", "Epic", "Legendary"], [0.2, 0.45, 0.3, 0.05]),
+            "boss": (["Rare", "Epic", "Legendary"], [0.25, 0.55, 0.2])
+        }
 
-    def generate_accesory(self):
-        pass
+    def generate_item(self, difficulty_key, level, templates=(armor_templates_list + weapon_templates_list)):
+        rarity_table = self.difficulty_table[difficulty_key]
+        rarity = random.choices(rarity_table[0], weights=rarity_table[1], k=1)[0]
+        prefixes_lst = self.rarity_affix[rarity]
+        prefix_dict = random.choice(prefixes_lst)
+        item_dict = random.choice(templates)
+        return Item(
+            item_stats_dict=item_dict, 
+            level=level, 
+            rarity=rarity, 
+            prefix_dict=prefix_dict
+        )
 
-    def generate_item(self):
-        pass
+    def generate_weapon(self, difficulty_key, level, templates=weapon_templates_list):
+        return self.generate_item(difficulty_key, level, templates)
+
+    def generate_armor(self, difficulty_key, level, templates=armor_templates_list):
+        return self.generate_item(difficulty_key, level, templates)
 
